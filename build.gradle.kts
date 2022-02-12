7f83f456f102a47668baf1057ac9cc6c77a3d9cd
chialu <mini801107@yahoo.com.tw>
2022-02-12 12:37:16 +0800

    TDD test and basic class

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,12 @@ repositories {
     mavenCentral()
 }
 
+val kotestVersion: String by project
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
 }
 
 tasks.test {
