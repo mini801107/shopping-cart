3e7fc1f281488edcbd75b8721a21e1232e39421a
chialu <mini801107@yahoo.com.tw>
2022-02-13 10:16:40 +0800

    Enable Gradle build scan plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "shopping-cart"
 
 plugins {
     id("com.gradle.enterprise") version "3.7"
+    id("nu.studer.build-scan.teamcity") version "1.1"
 }
 gradleEnterprise {
     buildScan {
