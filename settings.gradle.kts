45125958b64787307b05ed4f32a0b8cadbbb1a4c
chialu <mini801107@yahoo.com.tw>
2022-02-13 10:08:55 +0800

    Enable Gradle build scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,12 @@
 
 rootProject.name = "shopping-cart"
 
+plugins {
+    id("com.gradle.enterprise") version "3.7"
+}
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
